1363542bbbf1dc97a0d95fd671bd5d6ab6bd7e97
collins52 <collinsomonievusa@gmail.com>
2026-01-26 04:52:39 +0100

    Added functionality to day-list

--- AdminDashboard/script.js
+++ AdminDashboard/script.js
@@ -1,1 +1,21 @@
 // Main page scripts
+
+document.addEventListener('DOMContentLoaded', () =>{
+    handleDayClick();
+});
+
+function handleDayClick() {
+  const days = document.querySelectorAll('.day');
+
+  days.forEach(day => {
+    day.addEventListener('click', () => {
+      // remove active from all days
+      days.forEach(d => d.classList.remove('active'));
+
+      // add active to clicked day
+      day.classList.add('active');
+    });
+  });
+}
+
+
