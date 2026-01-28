12e23250b8d2f12c41ebfeb108f03f512f814ab9
collins52 <collinsomonievusa@gmail.com>
2026-01-28 19:44:50 +0100

    remove QR code attendance session logic from logout menu

--- AdminDashboard/script.js
+++ AdminDashboard/script.js
@@ -57,25 +57,6 @@ function logoutMenu() {
   });
 }
 
-// qr code
-const startAttendanceBtn = document.getElementById("startAttendance");
-const qrImg = document.getElementById("qrCode");
-
-startAttendanceBtn.addEventListener("click", () => {
-  const sessionId = Date.now().toString();
-
-  // Save session
-  localStorage.setItem("activeAttendanceSession", sessionId);
-
-  // Attendance page URL (this is what QR contains)
-  const attendanceURL = `../attendance/attendance.html?session=${sessionId}`;
-
-  // Generate QR
-  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(attendanceURL)}`;
-
-  alert("Attendance session started");
-});
-
 
 
 
